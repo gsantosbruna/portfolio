609294755ca1a5fc50842c495f96ab4d4a6d8791
gsantosbruna <118274503+gsantosbruna@users.noreply.github.com>
2023-05-21 23:50:20 +0200

    fix works image bug

--- js/conectResume.js
+++ js/conectResume.js
@@ -1,5 +1,7 @@
 async function getResume() {
-  const response = await fetch("http://localhost:3000/data");
+  const response = await fetch(
+    "https://raw.githubusercontent.com/gsantosbruna/portfolio/main/resume.json"
+  );
   const data = await response.json();
   return data;
 }
