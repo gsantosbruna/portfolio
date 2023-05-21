1734ccc1f163e0e275410ce2fedb783fd756231a
gsantosbruna <118274503+gsantosbruna@users.noreply.github.com>
2023-05-22 00:06:33 +0200

    using github api

--- js/conectResume.js
+++ js/conectResume.js
@@ -1,9 +1,18 @@
+const url =
+  "https://raw.githubusercontent.com/gsantosbruna/portfolio/main/resume.json";
+
 async function getResume() {
-  const response = await fetch(
-    "https://raw.githubusercontent.com/gsantosbruna/portfolio/main/resume.json"
-  );
-  const data = await response.json();
-  return data;
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Network response was not OK");
+    }
+
+    const data = await response.json();
+    return data.data;
+  } catch (error) {
+    console.error("Error retrieving JSON:", error);
+  }
 }
 
 export const conectResume = getResume;
